60957dccc17b00d8c9e06ec81a22580c47accba3
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-23 00:44:55 +0530

    Welcome Message

---
+++ src/SnakeLadder/snakeLadderProblem.java
@@ -0,0 +1,14 @@
+package SnakeLadder;
+
+public class snakeLadderProblem {
+/**
+ * 
+ * @param args
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Snakes & Ladders Game");
+		
+	}
+
+}
