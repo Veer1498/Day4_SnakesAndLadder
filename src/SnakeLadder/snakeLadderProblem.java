ca18b879318163caf9e5d1d988b908d934e0790c
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-23 13:45:52 +0530

    Rolling a Dice for a Number between 1-6

--- src/SnakeLadder/snakeLadderProblem.java
+++ src/SnakeLadder/snakeLadderProblem.java
@@ -8,7 +8,9 @@ public class snakeLadderProblem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Snakes & Ladders Game");
-		
+		int dice = (int)(Math.floor((Math.random()*6)+1));
+			System.out.println("Rolled Dice Number is "+ dice);
+			
 	}
 
 }
