b4018854523b610a9453730d09431de9486f70c1
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-23 14:39:04 +0530

    reaching Winning Position

--- src/SnakeLadder/snakeLadderProblem.java
+++ src/SnakeLadder/snakeLadderProblem.java
@@ -5,23 +5,29 @@ public class snakeLadderProblem {
  * 
  * @param args
  */
+	public static final int WINNING_POSITION =100;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Snakes & Ladders Game");
-		int playerPosition =0;
-		int dice = (int)(Math.floor((Math.random()*6)+1));
-		System.out.println("Rolled Dice Number is "+ dice);
-			
-		int chance = (int) Math.floor(Math.random()*3);
+	int playerPosition =0;
+	System.out.println("player position is " + playerPosition);
+		System.out.println("Roll Dice to Move");
+		while(playerPosition != WINNING_POSITION) {
+			int dice = (int)(Math.floor((Math.random()*6)+1));
+			int chance = (int) Math.floor(Math.random()*3);
 		if (chance == 0) {
 			System.out.println("No Play");
 			playerPosition = playerPosition;
-			System.out.println("player Actual Position is " + playerPosition);
 		}
 		else if (chance == 1) {
 			System.out.println("Got a Ladder");
-			playerPosition = playerPosition + dice;
-			System.out.println("player Actual Position is " + playerPosition);	
+			if (playerPosition < WINNING_POSITION ) {
+				playerPosition = playerPosition + dice;
+			}
+			else {
+				playerPosition = playerPosition;
+			}
 		}
 		else {
 			System.out.println("Hit with a Snake");
@@ -29,8 +35,9 @@ public class snakeLadderProblem {
 			if (playerPosition < 0 ) {
 				playerPosition =0;
 			}
+		}
 			System.out.println("player actual Position is " + playerPosition);
 		}
 	}
 
-}
+} 
